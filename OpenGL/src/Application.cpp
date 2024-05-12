8e74ed6416742dc86732880f66fa482cea576a7a
sgw <15173235626@163.com>
2024-05-12 22:51:40 +0800

    p99 add glEnableVertexAttribArray and glVertexAttribPointer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -45,6 +45,14 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions,GL_STATIC_DRAW);
 
+    //p99 start
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    //p99end
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //(const void*)8;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
